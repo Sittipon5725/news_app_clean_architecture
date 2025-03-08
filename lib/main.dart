7ea624b0d766cc465e5dd7a9280e8ac7616f7ba7
NickSittipon <sittipon5725@gmail.com>
2025-03-08 15:22:59 +0700

    kkgkkgkgkgkkgkglkgkglkglk

--- lib/main.dart
+++ lib/main.dart
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the buttoghghghghn thisnnnnn many timesgggggg:',
+              'You have pushed the buttoghghghgghhhfhfhggggggggggghn thisnnnnn many timesgggggg:',
             ),
             Text(
               '$_counter',
